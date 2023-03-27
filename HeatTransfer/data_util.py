ebf4ca16519b3d5152ceff8b61164fea8460df9b
Mattia Sotgia <mattiasotgia01@gmail.com>
2023-03-27 13:33:20 +0200

    data util/local changes

--- HeatTransfer/data_util.py
+++ HeatTransfer/data_util.py
@@ -20,7 +20,7 @@ class Data:
         self.pulse_duration = pulse_duration
         self.freq_sampling = 1/(self.time[1]-self.time[0])
         self.samples = len(self.time)
-        self.name = f'{self.samples} samples, {self.freq_sampling}Hz, {self.pulse_duration}s pulse'
+        self.name = f'samples/freq./pulse dur.: {self.samples}/{self.freq_sampling:.1f}Hz/{self.pulse_duration:.1f}s'
     
     def __str__(self):
         return f'{self.name}'
@@ -31,7 +31,8 @@ class Data:
         PT100ax = fig.add_subplot(gs[4])
         PT100ax.plot(self.time, self.PT100, 'r')
         if xlimUp is not None or xlimLo is not None:
-            PT100ax.set_xlim(xlimLo, xlimUp)
+            # PT100ax.set_xlim(xlimLo, xlimUp)
+            pass
         PT100ax.set_xlabel('Time (s)')
         PT100ax.set_ylabel('PT100 (V)')
         RISCax = fig.add_subplot(gs[3], sharex=PT100ax)
@@ -42,7 +43,11 @@ class Data:
         TCax.plot(self.time, self.TC, 'r', label=self.name)
         TCax.set_ylabel('TC (V)')
         TCax.tick_params(labelbottom=False)
-        TCax.text(0.95, 0.1, f'pulse duration: {self.pulse_duration}\nsamples: {self.samples}\nsampling freq.: {self.freq_sampling:.1f} Hz',
-                  ha='right', transform=TCax.transAxes)
+        # TCax.text(0.95, 0.1, f'pulse duration: {self.pulse_duration}\nsamples: {self.samples}\nsampling freq.: {self.freq_sampling:.1f} Hz',
+        #           ha='right', transform=TCax.transAxes)
         TCax.legend()
         hep.label.exp_text('L3','Preliminary', ax=TCax)
+        if xlimUp is not None:
+            loLim = xlimLo if xlimLo is not None else 0
+            for ax in [PT100ax, RISCax, TCax]:
+                ax.axvspan(loLim, xlimUp, edgecolor='k', facecolor='gray')
